6c61238ffb3cc600e5a8995a211052d7fcc4fc4f
jessi-17 <jaspreet.bhagat17@gmail.com>
2025-02-18 20:52:27 +0530

    add all the items

---
+++ Javascript/script.js
@@ -0,0 +1,24 @@
+console.log('this istext')
+console.warn("this is warning")
+console.error("this is error")
+
+
+
+
+//container where we will store our data
+//declaration 
+var a 
+//initialisation
+a = 10;
+console.log(a)
+//re-intialisation
+a = 30
+console.log(a)
+
+
+alert("this is alert!")
+
+confirm(['are you ok'])
+
+prompt("enter ur name")
+//we can also store these values
